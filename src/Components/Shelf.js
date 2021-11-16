f4779c2ec56355be334e3cefef05111a0e3f0d86
Mohammad <moh.osama84x@gmail.com>
2021-11-16 19:30:58 +0200

    Rendering home page with shelves

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-
+import Book from './Book'
 export default class Shelf extends Component {
     render() {
         return (
             <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <h2 className="bookshelf-title">{this.props.title}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
+                      <Book></Book>
+                      
                       <li>
                         <div className="book">
                           <div className="book-top">
