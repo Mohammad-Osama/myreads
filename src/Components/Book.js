cb87d92f71c0c8c27ef476ff104de861cd1e33d8
Mohammad <moh.osama84x@gmail.com>
2021-11-17 17:00:25 +0200

    Added ShelfChanger component to a Book

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ShelfChanger from './ShelfChanger'
 
 export default class Book extends Component {
     render() {
@@ -8,15 +9,7 @@ export default class Book extends Component {
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
+                            <ShelfChanger></ShelfChanger>
                           </div>
                           <div className="book-title">{this.props.title}</div>
                           <div className="book-authors">{this.props.authors}</div>
